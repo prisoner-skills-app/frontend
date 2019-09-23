622e2b9b5b78e309fd4370c945281702f8dc2d8d
Lucas Bazemore <lucasabazemore@gmail.com>
2019-09-23 17:40:03 -0500

    create folder and file

---
+++ client/src/scenes/AllCandidates/index.js
@@ -0,0 +1,12 @@
+import React from './react';
+
+//Components
+const Header = () => <h1>Header</h1>;
+
+//
+
+const AllCandidates = () => {
+    return <h1>All Candidates</h1>;
+};
+
+export default AllCandidates;
