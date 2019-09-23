8ab124dab79092e2f30713872b7a8bd99a1e8c8a
Lucas Bazemore <lucasabazemore@gmail.com>
2019-09-23 14:48:41 -0500

    added dumby components and routes for front page

--- client/src/scenes/App/index.js
+++ client/src/scenes/App/index.js
@@ -12,10 +12,31 @@ import { useStateValue } from '../../state';
 
 //Styled Components CSS
 
+//Dumby Components for Routes
+const UserAccount = () => <h1>User (Prison) Account</h1>;
+const AllCandidates = () => <h1>All Candidates</h1>;
+const AllPrisons = () => <h1>All Prisons</h1>;
+const PrisonProfile = () => <h1>Prison Profile Route</h1>;
+const CandidateProfile = () => <h1>Candidate Profile Route</h1>;
+const Login = () => <h1>Login / Sign up</h1>;
+const SignUp = () => <h1>Onboarding</h1>;
+
 const App = () => {
     return (
         <BrowserRouter>
             <Header as="h1" content="Prisoner Skills" />
+            <Switch>
+                <PrivateRoute path="/me" component={UserAccount} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/onboarding" component={SignUp} />
+                <Route exact path="/candidates" component={AllCandidates} />
+                <Route exact path="/prisons" component={AllPrisons} />
+                <Route
+                    path="/:prison-name/:candidate-id"
+                    component={CandidateProfile}
+                />
+                <Route exact path="/:prison-name" component={PrisonProfile} />
+            </Switch>
         </BrowserRouter>
     );
 };
@@ -27,7 +48,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
         <Route
             {...rest}
             render={props =>
-                true ? <Component {...props} /> : <Redirect to="/me" />
+                true ? <Component {...props} /> : <Redirect to="/login" />
             }
         />
     );
