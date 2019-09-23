07ffcf2d4d21c828fc89f602214f594da7494c15
Lucas Bazemore <lucasabazemore@gmail.com>
2019-09-23 15:52:44 -0500

    add in NavBar Menu Items

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,33 @@
 import React, { useState } from 'react';
 
 //Components
-import { SideBar, Menu } from 'semantic-ui-react';
+import { Sidebar, Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = ({ children }) => {
     const [visible, setVisible] = useState(false);
 
     return (
-        <SideBar.Pushable>
-            <SideBar
+        <Sidebar.Pushable>
+            <Sidebar
                 as={Menu}
                 animation="overlay"
                 visible={false}
                 direction="top"
                 onHide={() => setVisible}
-            ></SideBar>
-        </SideBar.Pushable>
+            ></Sidebar>
+            <Sidebar.Pusher>
+                <Menu attached="top">
+                    <Menu.Item>Prisoner Skills</Menu.Item>
+                    <Menu.Item>Prisoner Skills</Menu.Item>
+                    <Menu.Item>Prisoner Skills</Menu.Item>
+                    <Menu.Menu position="right">
+                        <Menu.Item>Login / Sign Up</Menu.Item>
+                    </Menu.Menu>
+                </Menu>
+                {children}
+            </Sidebar.Pusher>
+        </Sidebar.Pushable>
     );
 };
 
