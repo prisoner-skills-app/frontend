29f235fe4bcedb082ca222bfc89e7c9490b99fd1
Lucas Bazemore <lucasabazemore@gmail.com>
2019-09-23 10:43:44 -0500

    folder structure and initialization

---
+++ client/src/scenes/App/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+
+//Components
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+//Custom Components
+
+//State
+
+//Styled Components CSS
+
+const App = () => {
+    return (
+        <div>
+            <h1>Prisoner Skills</h1>
+        </div>
+    );
+};
+
+export default App;
