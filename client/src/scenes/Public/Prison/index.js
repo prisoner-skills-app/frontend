7182ce812de8dfbc723579599a9f392e89c2eb67
Lucas Bazemore <lucasabazemore@gmail.com>
2019-09-24 13:27:31 -0500

    Add in container and test cards
    Allowing me to create the side-by-side view

--- client/src/scenes/Public/Prison/index.js
+++ client/src/scenes/Public/Prison/index.js
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+import { medium } from '../../../globals/styles.js';
 
 //Components
 import { ColumnContainer } from '../../../globals/components';
+import { Card } from 'semantic-ui-react';
 
 //Dummy Components
 const Header = () => <h1>Header</h1>;
 const Prisoners = () => <h1>Prisoners</h1>;
+
+//Styled Components
+const CandidatesContainer = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+    padding: ${medium};
+`;
 
 const PrisonProfile = ({
     prisonName = 'Corrections',
@@ -15,11 +26,12 @@ const PrisonProfile = ({
     match,
 }) => {
     const [isLoading, setIsLoading] = useState(false);
-    const [prison, setPrison] = useState({});
+    const [prison, setPrison] = useState([]);
 
     useEffect(() => {
-        let prisonId = location.pathname;
-        let url = `api/prions/${prisonId}`;
+        //let prisonId = location.pathname;
+        //let url = `api/prions/${prisonId}`;
+        let url = 'https://my.api.mockaroo.com/users.json?key=ee167170';
 
         axios
             .get(url)
@@ -40,7 +52,21 @@ const PrisonProfile = ({
     return (
         <ColumnContainer>
             <Header title={prisonName} />
-            <Prisoners />
+            <CandidatesContainer>
+                {isLoading ? (
+                    <h1>Loading</h1>
+                ) : (
+                    prison.map(candidate => {
+                        return (
+                            <Card
+                                header={`${candidate.first_name} ${candidate.last_name}`}
+                                description={candidate.description}
+                                meta={candidate.skills}
+                            />
+                        );
+                    })
+                )}
+            </CandidatesContainer>
         </ColumnContainer>
     );
 };
