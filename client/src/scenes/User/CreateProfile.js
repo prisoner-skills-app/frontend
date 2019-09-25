7140a6b64b341cb8f1db7f9c1457add2f0bdb7f9
Lucas Bazemore <lucasabazemore@gmail.com>
2019-09-25 09:25:36 -0500

    add tabs for profile view

--- client/src/scenes/User/CreateProfile.js
+++ client/src/scenes/User/CreateProfile.js
@@ -5,13 +5,31 @@ import { small } from '../../globals/styles.js';
 
 //Components
 import { ColumnContainer, RowContainer } from '../../globals/components';
-import { Card, Grid, Responsive } from 'semantic-ui-react';
+import { Card, Grid, Responsive, Tab } from 'semantic-ui-react';
 import { Route, Link } from 'react-router-dom';
 
 //Dummy Components
 const Header = () => <h1>Header</h1>;
 const CreateForm = () => <h1>Create Form</h1>;
-const Preview = () => <h1>Preview Form</h1>;
+const Preview = () => {
+    const panes = [
+        {
+            menuItem: 'Card',
+            render: () => <Tab.Pane attached={false}>Tab 1 Content</Tab.Pane>,
+        },
+        {
+            menuItem: 'Full Profile',
+            render: () => <Tab.Pane attached={false}>Tab 2 Content</Tab.Pane>,
+        },
+    ];
+
+    return (
+        <>
+            <h1>How Profile Will Look</h1>
+            <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
+        </>
+    );
+};
 
 //Styled Components
 
