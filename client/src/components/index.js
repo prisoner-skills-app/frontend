1ecb8667cfa049c9d5e74ad61dbebb8891e018f5
Luis Ocasio <luisocasio03@gmail.com>
2019-09-25 23:16:41 -0400

    snackbar styled complete.

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,6 +4,7 @@ import onboardingForm from './OnboardingForm';
 import header from './Header';
 import signUp from './SignUp';
 import newProfile from './NewProfile';
+import onBoardingComplete from "./OnboardingComplete";
 import accountSettingsForm from './AccountSettingsForm';
 
 export const NavBar = navBar;
@@ -12,4 +13,5 @@ export const OnboardingForm = onboardingForm;
 export const Header = header;
 export const SignUp = signUp;
 export const NewProfile = newProfile;
+export const OnboardingComplete = onBoardingComplete;
 export const AccountSettingsForm = accountSettingsForm;
